0e97c7bb1df9784f6aa6b4c757bb0a00b30979ee
Ruaraidheu <ruaraidh64@gmail.com>
2017-11-20 22:14:55 +0000

    Added Games/money

--- reulib/Games/Money.cs
+++ reulib/Games/Money.cs
@@ -9,5 +9,18 @@ namespace Ruaraidheulib.Games
     public class Money
     {
         long val;
+        string symb = "$";
+        public Money()
+        {
+
+        }
+        public Money(string symbol)
+        {
+            symb = symbol;
+        }
+        public string Getstring()
+        {
+            return symb + val.PadToString(0, 2);
+        }
     }
 }
